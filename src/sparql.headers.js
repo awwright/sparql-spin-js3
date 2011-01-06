0a399ec58a6bb1c4e23f2d1f1ecfad9617e481a8
Austin Wright <diamondmagic@users.sourceforge.net>
2011-01-06 03:34:18 -0500

    Clean up syntax, add function to turn Array into a RDF collection

--- src/sparql.headers.js
+++ src/sparql.headers.js
@@ -2,4 +2,9 @@ var js3 = require('js3');
 js3.curiemap.sp = "http://spinrdf.org/sp#";
 js3.propertymap.sp = ['where', 'resultVariables', 'resultNodes', 'templates', 'graphIRI', 'deletePattern', 'insertPattern', 'varName', 'expression', 'subject', 'predicate', 'path', 'object', 'variable', 'elements', 'graphNameNode', 'query', 'serviceURI'];
 
+function rdfCollection(v){
+	return v.toList();
+}
+
+module.exports=
 // @include "sparql.parseroutput.js"
