ff29e28a338656a064e72d1af8f41a128d6e126e
Austin Wright <diamondmagic@users.sourceforge.net>
2011-03-06 17:56:44 -0700

    Remove dependency on the js3 library

--- src/sparql.headers.js
+++ src/sparql.headers.js
@@ -1,9 +1,13 @@
-var js3 = require('js3');
-js3.curiemap.sp = "http://spinrdf.org/sp#";
-js3.propertymap.sp = ['where', 'resultVariables', 'resultNodes', 'templates', 'graphIRI', 'deletePattern', 'insertPattern', 'varName', 'expression', 'subject', 'predicate', 'path', 'object', 'variable', 'elements', 'graphNameNode', 'query', 'serviceURI'];
+try{
+	var js3 = require('js3');
+	js3.curiemap.sp = "http://spinrdf.org/sp#";
+	js3.propertymap.sp = ['where', 'resultVariables', 'resultNodes', 'templates', 'graphIRI', 'deletePattern', 'insertPattern', 'varName', 'expression', 'subject', 'predicate', 'path', 'object', 'variable', 'elements', 'graphNameNode', 'query', 'serviceURI'];
+}catch(e){}
 
+// Label an Array as an RDF Collection and
+// not just multiple objects to the same predicate
 function rdfCollection(v){
-	return v.toList();
+	return v.toList?v.toList()||v;
 }
 
 module.exports=
